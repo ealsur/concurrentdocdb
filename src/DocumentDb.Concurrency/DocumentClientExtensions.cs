7f0fbc40220bc75b8ee0ed45c31cff26b4b5e1ab
ealsur <ealsur@hotmail.com>
2017-03-21 16:53:09 -0300

    Drafting

---
+++ src/DocumentDb.Concurrency/DocumentClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DocumentDb.Concurrency
+{
+    public static class DocumentClientExtensions
+    {
+		public static Task<ResourceResponse<Document>> ReplaceConcurrentDocumentAsync(this DocumentClient client, Document document, RequestOptions options = null)
+		{
+			RequestOptions refOptions = options ?? new RequestOptions();
+			refOptions.AccessCondition = new AccessCondition() { Type = AccessConditionType.IfMatch, Condition = document.ETag };
+			try
+			{
+				return client.ReplaceDocumentAsync(document, refOptions);
+			}
+			catch (DocumentClientException dce)
+			{
+				if (dce.StatusCode == HttpStatusCode.PreconditionFailed)
+				{
+					//Handle concurrency error
+				}
+				throw;
+			}
+		}
+	}
+}
