91a3e9772c7efde94268113e8d0a6fa2827a089c
ealsur <ealsur@hotmail.com>
2017-03-23 22:10:30 -0300

    Test now creates db and collection

--- test/DocumentDb.Concurrency.Tests/ConcurrentTests.cs
+++ test/DocumentDb.Concurrency.Tests/ConcurrentTests.cs
@@ -43,17 +43,32 @@ namespace DocumentDb.Concurrenty.Tests
 			return UriFactory.CreateDocumentCollectionUri("test", "concurrencyTest");
 		}
 
-		/// <summary>
-		/// Creates a Query with FeedOptions
-		/// </summary>
-		/// <typeparam name="T">Type of Class to serialize</typeparam>
-		/// <param name="feedOptions"></param>
-		/// <returns></returns>
-		private IQueryable<T> CreateQuery<T>(FeedOptions feedOptions = null)
+
+		private Uri GetDocumentLink(string theId)
 		{
-			return _dbClient.CreateDocumentQuery<T>(_collectionUri, feedOptions);
+			return UriFactory.CreateDocumentUri("test", "concurrencyTest", theId);
 		}
-		
+
+		private async Task<Database> GetOrCreateDatabaseAsync(string id)
+		{
+			// Get the database by name, or create a new one if one with the name provided doesn't exist.
+			// Create a query object for database, filter by name.
+			IEnumerable<Database> query = from db in _dbClient.CreateDatabaseQuery()
+										  where db.Id == id
+										  select db;
+
+			// Run the query and get the database (there should be only one) or null if the query didn't return anything.
+			// Note: this will run synchronously. If async exectution is preferred, use IDocumentServiceQuery<T>.ExecuteNextAsync.
+			Database database = query.FirstOrDefault();
+			if (database == null)
+			{
+				// Create the database.
+				database = await _dbClient.CreateDatabaseAsync(new Database { Id = id });
+			}
+
+			return database;
+		}
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -61,6 +76,10 @@ namespace DocumentDb.Concurrenty.Tests
 		[Fact]
 		public async Task TheOneAndOnly()
 		{
+			//Creates db and collection if they dont exist
+			var database = await GetOrCreateDatabaseAsync("test");
+			DocumentCollection c1 = await _dbClient.CreateDocumentCollectionIfNotExistsAsync(database.SelfLink, new DocumentCollection { Id = "concurrencyTest" });
+
 			//Creates a random document
 			var theId = Guid.NewGuid().ToString();
 			var aDoc = new DummyDocument();
@@ -68,17 +87,17 @@ namespace DocumentDb.Concurrenty.Tests
 			await _dbClient.CreateDocumentAsync(_collectionUri, aDoc);
 			
 			//Obtains the created document
-			Document theDoc = await _dbClient.ReadDocumentAsync(UriFactory.CreateDocumentUri("test", "concurrencyTest", theId));
+			Document theDoc = await _dbClient.ReadDocumentAsync(GetDocumentLink(theId));
 			Assert.Equal(theDoc.Id, theId);
 
 			//Modifies document altering ETag internally
 			theDoc.SetPropertyValue("Property1", "modified");
-			var result = await _dbClient.ReplaceConcurrentDocumentAsync(UriFactory.CreateDocumentUri("test", "concurrencyTest", theDoc.Id),theDoc);
+			var result = await _dbClient.ReplaceConcurrentDocumentAsync(GetDocumentLink(theDoc.Id),theDoc);
 			Assert.Equal(result.StatusCode, HttpStatusCode.OK);
 
 			//Modifies the same document generating a concurrency exception and handling it
 			theDoc.SetPropertyValue("Property1", "modified again");
-			var result2 = await _dbClient.ReplaceConcurrentDocumentAsync(UriFactory.CreateDocumentUri("test", "concurrencyTest", theDoc.Id), theDoc).OnConcurrencyException((exception)=>
+			var result2 = await _dbClient.ReplaceConcurrentDocumentAsync(GetDocumentLink(theDoc.Id), theDoc).OnConcurrencyException((exception)=>
 			{
 				Console.WriteLine($"I got it!! {exception.Message}");
 			});
